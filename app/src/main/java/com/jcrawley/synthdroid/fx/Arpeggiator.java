32c3953efa6fda95d5b07881eb6a086b94a6acb0
john <johncrawley1@gmail.com>
2023-08-11 23:58:31 +0100

    Created an arpeggiator class to change notes on a given interval.

---
+++ app/src/main/java/com/jcrawley/synthdroid/fx/Arpeggiator.java
@@ -0,0 +1,79 @@
+package com.jcrawley.synthdroid.fx;
+
+import com.jcrawley.synthdroid.Interval;
+import com.jcrawley.synthdroid.MainActivity;
+import com.jcrawley.synthdroid.MusicNote;
+import com.jcrawley.synthdroid.NoteCalculator;
+import com.jcrawley.synthdroid.view.NoteItem;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
+
+public class Arpeggiator {
+
+    private int rate = 300;
+    private boolean isEnabled = true;
+    private ScheduledFuture<?> future;
+    private final ScheduledExecutorService executorService;
+    private final List<MusicNote> notes;
+    private int currentNoteIndex;
+    private final NoteCalculator noteCalculator;
+    private final MainActivity activity;
+
+
+    public Arpeggiator(MainActivity mainActivity){
+        executorService = Executors.newSingleThreadScheduledExecutor();
+        notes = new ArrayList<>();
+        noteCalculator = new NoteCalculator();
+        this.activity = mainActivity;
+    }
+
+
+    public void setEnabled(boolean isEnabled){
+        this.isEnabled = isEnabled;
+    }
+
+
+    public void stop(){
+        future.cancel(false);
+    }
+
+
+    public void start(MusicNote note){
+        setNoteFrequency(note);
+        if(!isEnabled){
+            return;
+        }
+        currentNoteIndex = 0;
+        calculateIntervals(note);
+        future = executorService.scheduleAtFixedRate(this::changeNote,0 , rate, TimeUnit.MILLISECONDS);
+    }
+
+
+    private void calculateIntervals(MusicNote note){
+        notes.add(note);
+        notes.add(noteCalculator.addIntervalUp(note, Interval.MAJOR_THIRD));
+        notes.add(noteCalculator.addIntervalUp(note, Interval.PERFECT_FIFTH));
+    }
+
+
+    public void changeNote(){
+        incrementCurrentNoteIndex();
+        setNoteFrequency(notes.get(currentNoteIndex));
+    }
+
+
+    private void setNoteFrequency(MusicNote musicNote){
+        activity.setFrequency(musicNote.getFrequency());
+    }
+
+
+    private void incrementCurrentNoteIndex(){
+        currentNoteIndex = (currentNoteIndex + 1) % notes.size();
+    }
+
+}
